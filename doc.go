c8ce83fa4aa5ce5b65150d01bacf8c397fb8967c
James Lucktaylor <jlucktay+github@gmail.com>
2019-07-12 15:55:23 +0100

    Add godoc and goreportcard badges.

---
+++ doc.go
@@ -0,0 +1,3 @@
+// Package stack contains a main binary 'stack' (mostly config and flag parsing) and a 'common' package (the rest of
+// the logic) which constite a helper tool for working with Terraform stacks attached to Azure DevOps pipelines.
+package stack
