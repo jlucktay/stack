a69f19dd6f189e38bcf3d443bf7737be8100b906
James Lucktaylor <jlucktay+github@gmail.com>
2019-07-14 10:48:36 +0100

    Fiddle with 'google/go-github' prototype.

--- cmd/stack/issue.go
+++ cmd/stack/issue.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 
+	"github.com/google/go-github/v26/github"
 	"github.com/jlucktay/stack/pkg/common"
+	"github.com/pkg/errors"
 	"github.com/spf13/viper"
+	"golang.org/x/oauth2"
 )
 
 // issue flow:
@@ -31,5 +35,36 @@ func createIssue(text ...string) {
 	fmt.Printf("stackPath: '%s'\n", stackPath)
 
 	// 1
-	// TODO
+	// WIP
+
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{
+			AccessToken: viper.GetString("github.pat"),
+		},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+
+	issueRequest := &github.IssueRequest{
+		Title: github.String("hello world"),
+		Labels: &[]string{
+			stackPath,
+		},
+	}
+
+	// issue, response, errCreate := client.Issues.Create(
+	// 	ctx,
+	// 	viper.GetString("github.org"),
+	// 	viper.GetString("github.repo"),
+	// 	issueRequest,
+	// )
+	issue, response, errCreate := client.Issues.Create(ctx, "jlucktay", "stack", issueRequest)
+	if errCreate != nil {
+		log.Fatal(errors.Wrap(errCreate, "errCreate!\n"))
+	}
+
+	fmt.Printf("\nissue:\n\t'%s'\n", issue)
+	fmt.Printf("\nresponse:\n\t'%#v'\n", *response)
 }
