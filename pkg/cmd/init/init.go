7b3c4cba1484b28d23a1a1d351e92a793b0f372d
James Lucktaylor <jlucktay@users.noreply.github.com>
2019-09-28 15:08:03 +0100

    Docs 2019-09-28 (#55)
    * Update README and some subcommand contextual help.

--- pkg/cmd/init/init.go
+++ pkg/cmd/init/init.go
@@ -11,12 +11,9 @@ func NewCommand() *cobra.Command {
 	c := &cobra.Command{
 		Use:   "init",
 		Short: "Initialise this Terraform stack against remote state",
-		Long: `A longer description that spans multiple lines and likely contains examples
-	and usage of using your command. For example:
-
-	Cobra is a CLI library for Go that empowers applications.
-	This application is a tool to generate the needed files
-	to quickly create a Cobra application.`,
+		Long: `Runs Terraform with values derived from configured settings to initialise the
+current stack directory, using the Azure storage account for the remote state
+backend.`,
 		Run: func(cmd *cobra.Command, args []string) {
 			common.InitStack()
 		},
