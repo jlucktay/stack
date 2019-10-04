185c3c5d9ba15471214b54f2393948c3ccdfe79b
James Lucktaylor <jlucktay@users.noreply.github.com>
2019-10-04 18:25:35 +0100

    Fix 'built by' in 'stack version'. (#58)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -7,9 +7,9 @@ import (
 // Take ldflags from GoReleaser
 var (
 	//nolint
-	version, commit, date, builtby string
+	version, commit, date, builtBy string
 )
 
 func Details() string {
-	return fmt.Sprintf("stack v%s from commit %s, built %s by %s.", version, commit, date, builtby)
+	return fmt.Sprintf("stack v%s from commit %s, built %s by %s.", version, commit, date, builtBy)
 }
