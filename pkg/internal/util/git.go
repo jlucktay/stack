b3f0cdede53a7928ea98ad881a3a79b1e9d2ee97
James Lucktaylor <jlucktay+github@gmail.com>
2020-01-04 20:39:37 +1100

    Refactor CurrentBranch().
    - panic when not inside a git repo directory
    - return an empty string under specific error conditions
    - needs further handling/refactoring downstream when this func is called

--- pkg/internal/util/git.go
+++ pkg/internal/util/git.go
@@ -2,19 +2,44 @@ package util
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
 	"strings"
+	"syscall"
 )
 
-// CurrentBranch parses out the current git branch.
+// CurrentBranch parses out the current git branch, if we are inside a git repo.
+// Otherwise, an empty string is returned.
 func CurrentBranch() (s string) {
-	gitRaw, errExec := exec.Command("git", "rev-parse", "--abbrev-ref", "HEAD").Output()
-	if errExec == nil {
-		s = strings.TrimSpace(string(gitRaw))
-	} else {
-		panic(errExec)
+	cmdGit := exec.Command("git", "rev-parse", "--is-inside-work-tree")
+	gitRaw, errExec := cmdGit.Output()
+
+	// With thanks to:
+	// https://stackoverflow.com/questions/10385551/get-exit-code-go
+	if errExec != nil {
+		if errExit, ok := errExec.(*exec.ExitError); ok {
+			// The program has exited with an exit code != 0
+			//
+			// This works on both Unix and Windows. Although package
+			// syscall is generally platform dependent, WaitStatus is
+			// defined for both Unix and Windows and in both cases has
+			// an ExitStatus() method with the same signature.
+			if status, ok := errExit.Sys().(syscall.WaitStatus); ok {
+				exitCode := status.ExitStatus()
+				if exitCode == 128 {
+					return
+				}
+
+				log.Fatalf("'%+v' exit code: %d", cmdGit, exitCode)
+			}
+		} else {
+			log.Fatalf("'%+v': %+v", cmdGit, errExec)
+		}
 	}
+
+	s = strings.TrimSpace(string(gitRaw))
+
 	return
 }
 
