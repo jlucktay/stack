9c61a285a6f9352a0dc031dc1a952cea785da1ac
James Lucktaylor <jlucktay+github@gmail.com>
2020-07-16 09:35:37 +0100

    build(goreleaser): add Go version to binary details

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -6,10 +6,9 @@ import (
 
 // Take ldflags from GoReleaser.
 var (
-	//nolint
-	version, commit, date, builtBy string
+	version, commit, goVersion, date, builtBy string //nolint:gochecknoglobals
 )
 
 func Details() string {
-	return fmt.Sprintf("stack v%s from commit %s, built %s by %s.", version, commit, date, builtBy)
+	return fmt.Sprintf("stack %s built from commit %s with %s on %s by %s.", version, commit, goVersion, date, builtBy)
 }
