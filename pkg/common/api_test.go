ca9b39237abfa3b74e3685f3426893c1b9d39e6a
James Lucktaylor <jlucktay+github@gmail.com>
2021-05-13 07:23:05 +0100

    refactor(pkg/common): rearrange a test

--- pkg/common/api_test.go
+++ pkg/common/api_test.go
@@ -14,25 +14,21 @@ import (
 func TestBuildRequest(t *testing.T) {
 	is := is.New(t)
 
-	handler := func(_ http.ResponseWriter, r *http.Request) {
-		auth := r.Header.Get("Authorization")
-		is.True(strings.HasPrefix(auth, "Basic"))                  // 'Authorization' header value starts with 'Basic'.
-		is.Equal("application/json", r.Header.Get("Content-Type")) // 'Content-Type' == 'application/json'.
-	}
-
-	var testBuildID uint = 123
-
-	payload, errPayload := common.GetPostPayload(testBuildID, map[string]string{"key": "value"}, "branch")
-	if errPayload != nil {
-		t.Fatal(errPayload)
-	}
+	payload, errPayload := common.GetPostPayload(123, map[string]string{"key": "value"}, "branch")
+	is.NoErr(errPayload)
 
 	req, errCreate := common.CreateBuildRequest(
 		"dev.azure.com/build/me/a/build",
 		"this is my PAT; there are many like it, but this one is mine",
 		payload)
-	if errCreate != nil {
-		t.Fatal(errCreate)
+	is.NoErr(errCreate)
+
+	handler := func(_ http.ResponseWriter, r *http.Request) {
+		auth := r.Header.Get("Authorization")
+		is.True(strings.HasPrefix(auth, "Basic")) // 'Authorization' header value starts with 'Basic'.
+
+		ct := r.Header.Get("Content-Type")
+		is.Equal("application/json", ct) // 'Content-Type' == 'application/json'.
 	}
 
 	w := httptest.NewRecorder()
